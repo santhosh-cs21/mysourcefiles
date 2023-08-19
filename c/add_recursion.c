62e2b4595f865e518bcffb086e4e7b892e4be345
santhosh-cs21 <santhosh-cs21>
2023-08-19 17:33:53 +0000

    1user

---
+++ c/add_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int add(int,int);
+int main()
+{
+    int x,y,result;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&x,&y);
+    result=add(x,y);
+    printf("Sum of two numbers are: %d\n",result);
+    return 0;
+}
+int add(int x, int y)
+{
+    if(y==0)
+        return x;
+    else
+        return(1+add(x,y-1));
+}
